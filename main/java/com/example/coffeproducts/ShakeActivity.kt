c691ee8c6b905cefce15c967cb6c2eb74ec3e410
sandakirani <vihangisandakirani@gmail.com>
2025-01-13 12:58:22 +0530

    Header updated

---
+++ main/java/com/example/coffeproducts/ShakeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.coffeproducts
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ShakeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
