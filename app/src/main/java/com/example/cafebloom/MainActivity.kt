76ee5bc15fb6c98d21ca34ab3318faea87cc7ed9
geenath-hettiarachchi <dls.geenath@gmail.com>
2025-01-25 15:52:30 +0530

    update

--- app/src/main/java/com/example/cafebloom/MainActivity.kt
+++ app/src/main/java/com/example/cafebloom/MainActivity.kt
@@ -10,14 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        val scrollButton = findViewById<Button>(R.id.processing)
-        val horizontalScrollView = findViewById<HorizontalScrollView>(R.id.horizontalScrollView)
-
-        scrollButton.setOnClickListener {
-            // Scroll right by 200 pixels
-            horizontalScrollView.smoothScrollBy(200, 0)
-        }
     }
 }
