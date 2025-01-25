98bda98d855d9648ebd74a853822e55e0ddb87a8
Bladerunner1999 <ravinduanjana437@gmail.com>
2025-01-25 12:39:35 +0530

    my_orders

--- app/src/main/java/com/example/cafebloom/MainActivity.kt
+++ app/src/main/java/com/example/cafebloom/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.cafebloom
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.graphics.Color
+import android.widget.Button
+import android.widget.HorizontalScrollView
+import android.widget.LinearLayout
+import android.widget.ScrollView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.my_orders)
+
+
+        val scrollButton = findViewById<Button>(R.id.processing)
+        val horizontalScrollView = findViewById<HorizontalScrollView>(R.id.horizontalScrollView)
+
+        scrollButton.setOnClickListener {
+            // Scroll right by 200 pixels
+            horizontalScrollView.smoothScrollBy(200, 0)
+        }
     }
 }
