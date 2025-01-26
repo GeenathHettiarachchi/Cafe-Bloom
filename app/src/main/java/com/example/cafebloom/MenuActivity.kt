f5da8ec66dfc0b3f978badf6c83c84cac79b5323
geenath-hettiarachchi <dls.geenath@gmail.com>
2025-01-26 15:41:28 +0530

    create menu fragment

--- app/src/main/java/com/example/cafebloom/MenuActivity.kt
+++ app/src/main/java/com/example/cafebloom/MenuActivity.kt
@@ -2,13 +2,11 @@ package com.example.cafebloom
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.HorizontalScrollView
 
 
-class MainActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_menu)
     }
 }
